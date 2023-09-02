e727fa3889edb43ad7f7070bf8667c33414e610a
taku toriumi <tt.tuttofare@gmail.com>
2023-09-02 22:36:19 +0900

    [add]<RouteView>を<Suspense>でラップ

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header></header>
-  <RouterView />
+  <header>
+    <h2>abceedコーディング試験</h2>
+  </header>
+  <Suspense>
+    <RouterView />
+  </Suspense>
 </template>
 
 <style scoped>
