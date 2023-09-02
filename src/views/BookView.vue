df2e33cc4ba7af4d9f4bd20876e4aaf6c4a1a5e5
taku toriumi <tt.tuttofare@gmail.com>
2023-09-03 02:40:57 +0900

    [add]テストタイプのアイコンを仮実装

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -41,11 +41,20 @@ const topCateogyList: Array<TopCategory> = await fetchBookApi();
 const uniqueBookList: Array<Object> = getUniqueBookList(topCateogyList);
 const book: BookDetails = await findBookById(uniqueBookList, id)
 
-// const testTypes: Array<{
-//   name: string, icon: string
-// }> = [
-//   { name: 'アプリ学習', icon: '' }
-// ]
+// テストタイプ
+// 本来は書籍詳細オブジェクトに識別データ（配列またはboolean）が含まれていると思われる
+const testTypes: Array<{
+  label: string, icon: string
+}> = [
+  { label: 'アプリ学習', icon: 'icon_study_quiz.svg' },
+  { label: 'テスト', icon: 'icon_study_test.svg' },
+  { label: '音声(無料)', icon: 'icon_study_sound.svg' },
+  { label: 'SWトレ', icon: 'icon_study_sw.svg' },
+  { label: '単語一覧', icon: 'icon_study_vocab.svg' },
+  { label: 'マークシート', icon: 'icon_study_marksheet.svg' },
+  { label: '学習記録', icon: 'icon_study_record.svg' }
+
+]
 
 </script>
 
@@ -54,27 +63,41 @@ const book: BookDetails = await findBookById(uniqueBookList, id)
     <div class="c-bookdetails">
       <img class="c-bookdetails__cover" :src="book.imgUrl" :alt="book.title" />
       <div class="c-bookdetails__info">
-        <h2 class="c-bookdetails__info__title">{{ book.title }}</h2>
-        <dl class="c-bookdetails__info__item">
+        <h2 class="c-bookdetails__title">{{ book.title }}</h2>
+        <dl class="c-bookdetails__item">
           <dt>著者</dt>
           <dd>{{ book.author }}</dd>
         </dl>
-        <dl class="c-bookdetails__info__item">
+        <dl class="c-bookdetails__item">
           <dt>出版社</dt>
           <dd>{{ book.publisher }}</dd>
         </dl>
+        <div class="c-bookdetails__btn-wrapper">
+          <button class="c-bookdetails__btn">MyBooks追加</button>
+          <button class="c-bookdetails__btn --active">読み放題中</button>
+        </div>
       </div>
     </div>
-    <div class="c-test-types">
-      <ul>
-        <li></li>
-      </ul>
-    </div>
+    <ul class="c-test-types">
+      <li class="c-test-types__item" v-for="testType in testTypes" :key="testType">
+        <div class="c-test-types__item__inner">
+          <picture>
+            <source media="(prefers-color-scheme: dark)" :srcset="`/images/icons/dark/${testType.icon}`">
+            <img :src="`/images/icons/light/${testType.icon}`" alt="" class="c-test-types__item__icon">
+          </picture>
+          <div class="c-test-types__item__label">{{ testType.label }}</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <style lang="scss">
+.p-book {
+  background-color: #f5f5f5;
+}
 .c-bookdetails {
+  background-color: #fff;
   display: flex;
   width: 100%;
   max-width: 450px;
@@ -86,32 +109,88 @@ const book: BookDetails = await findBookById(uniqueBookList, id)
   }
   &__info {
     padding-left: 15px;
-    &__title {
-      font-size: 16px;
-      font-weight: 700;
-      line-height: 1.5;
-      margin-bottom: 10px;
+  }
+  &__title {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1.5;
+    margin-bottom: 10px;
+  }
+  &__item {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 10px;
+    dt {
+      font-size: 10px;
+      font-weight: 400;
+      height: 1.6em;
+      padding: 2px 6px;
+      color: #777;
+      background-color: #eee;
+      border-radius: 2px;
+      margin-right: 6px;
     }
-    &__item {
-      display: flex;
-      align-items: flex-start;
-      margin-bottom: 10px;
-      dt {
-        font-size: 10px;
-        font-weight: 400;
-        height: 1.6em;
-        padding: 2px 6px;
-        color: #777;
-        background-color: #eee;
-        border-radius: 2px;
-        margin-right: 10px;
-      }
-      dd {
-        font-size: 12px;
-        font-weight: 600;
-        padding: 1px 0;
-        color: #777;
-      }
+    dd {
+      font-size: 12px;
+      font-weight: 600;
+      padding: 1px 0;
+      color: #777;
+    }
+  }
+  &__btn {
+    display: inline-block;
+    width: calc((100% - 10px) / 2);
+    height: 20px;
+    text-align: center;
+    font-size: 10px;
+    font-weight: 700;
+    color: red;
+    border: 1px solid red;
+    border-radius: 5px;
+    &:first-of-type {
+      margin-right: 10px;
+    }
+    &.--active {
+      color: #fff;
+      background-color: red;
+    }
+  }
+}
+.c-test-types {
+  padding: 15px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  &__item {
+    width: 70px;
+    aspect-ratio: 1 / 1;
+    flex-shrink: 0;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    /* ボタンの中身を中央合わせ */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &__inner {
+      text-align: center;
+    }
+    &:nth-of-type(4n) {
+      margin-right: 0;
+    }
+    &:nth-last-of-type(-n + 4) {
+      margin-bottom: 0;
+    }
+    &__icon {
+      width: 40px;
+      height: 40px;
+      margin-bottom: 5px;
+    }
+    &__label {
+      font-size: 10px;
     }
   }
 }
