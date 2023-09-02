9ab4c2ae52349118851efc6addd84e20f7682484
taku toriumi <tt.tuttofare@gmail.com>
2023-09-02 22:38:44 +0900

    [add]書籍詳細のRouteを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import BookView from '@/views/BookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home'
+      name: '書籍一覧',
+      component: HomeView,
+      meta: { title: 'abceed | 書籍一覧', desc: '' }
+    },
+    {
+      path: '/book/',
+      name: '書籍詳細',
+      component:  BookView,
+      meta: { title: 'abceed | 書籍詳細', desc: '' }
     }
   ]
 })
