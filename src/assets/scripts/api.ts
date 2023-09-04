fdfd3aa551d5deb481338ac46eebbdbe2411ece8
taku toriumi <tt.tuttofare@gmail.com>
2023-09-04 12:16:50 +0900

    [clean]APIレスポンスの型定義をapi.tsにまとめる

--- src/assets/scripts/api.ts
+++ src/assets/scripts/api.ts
@@ -1,19 +1,32 @@
-// 書籍系API返り値
+// 書籍系APIの返り値を型定義しておく
+// /mock/book/all
 export type TopCategoryResponse = {
   id_top_category: string,
   name_category: string,
-  sub_category_list: {
-    id_category: string,
-    name_category: string,
-    book_list: Array<Object>
-  }
+  sub_category_list: Array<SubCategoryResponse>
 }
+// /mock/book/all > サブカテゴリー
+export type SubCategoryResponse = {
+  id_category: string,
+  name_category: string,
+  book_list: Array<BookResponse>
+}
+// /mock/book/all > サブカテゴリー > 書籍
+export type BookResponse = {
+  id_book: string,
+  name_book: string,
+  publisher: string,
+  author: string,
+  img_url: string
+}
+
+// 書籍系APIのフェッチの返り値
 export type TopCategory = {
   id: string,
   name: string,
-  subCategories: Object[]
+  subCategories: Array<SubCategoryResponse>
 }
-
+// 書籍系APIのフェッチ
 export const fetchBookApi = async (): Promise<TopCategory[]> => {
   const endpoint = "https://dev-app-api.abceed.com/mock/book/all"
   const res = await fetch(endpoint)
