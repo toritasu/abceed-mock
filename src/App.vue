d28917cd4f2ff358247394a4791ebcad36a66dad
taku toriumi <tt.tuttofare@gmail.com>
2023-09-04 11:22:35 +0900

    [update]ヘッダーをコンポーネント化

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import TheHeader from '@/components/common/TheHeader.vue';
 </script>
 
 <template>
-  <header class="c-header">
-    <div class="c-header__inner">
-      <h1>abceedコーディング試験</h1>
-    </div>
-  </header>
+  <TheHeader />
   <Suspense>
     <RouterView />
   </Suspense>
 </template>
 
 <style lang="scss" scoped>
-.c-header {
-  height: 50px;
-  background-color: #ddd;
-  &__inner {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 0 15px;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    h1 {
-      font-size: 18px;
-      font-weight: 700;
-    }
-  }
-}
 </style>
---
+++ src/components/common/TheHeader.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <header class="c-header">
+    <div class="c-header__inner">
+      <h1>abceedコーディング試験</h1>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.c-header {
+  height: 50px;
+  background-color: #ddd;
+  &__inner {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 0 15px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h1 {
+      font-size: 18px;
+      font-weight: 700;
+    }
+  }
+}
+</style>
