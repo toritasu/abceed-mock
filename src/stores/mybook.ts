b602ce5f617b5ca6786e32b25855b6f60c512cb1
taku toriumi <tt.tuttofare@gmail.com>
2023-09-04 16:20:03 +0900

    [clean]pinia設定ファイルのts記法を調査/修正

--- src/stores/mybook.ts
+++ src/stores/mybook.ts
@@ -1,23 +1,25 @@
 import { defineStore } from "pinia";
+import type { BookCard } from "@/assets/scripts/types.ts"
 
 export const useMybookStore = defineStore('mybook', {
   state: () => ({
-    books: []
+    mybooks: [] as Array<BookCard>
   }),
   getters: {
     isMybook(state) {
       return (bookId: string): boolean => {
-        return state.books.includes(bookId)
+        return state.mybooks
+          .find((book: BookCard) => book.id === bookId)
       }
     }
   },
   actions: {
-    add(bookId) {
-      this.books.push(bookId);
+    add(bookId: string) {
+      this.mybooks.push(bookId);
     },
-    remove(bookId) {
-      const index = this.books.indexOf(bookId);
-      this.books.splice(index, 1);
+    remove(bookId: string) {
+      const index = this.mybooks.indexOf(bookId);
+      this.mybooks.splice(index, 1);
     }
   }
 })
