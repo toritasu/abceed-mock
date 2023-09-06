a3d1fbce9fad9242477bd93bbe45438bb5b2cd2f
taku toriumi <tt.tuttofare@gmail.com>
2023-09-06 21:50:35 +0900

    [clean]書籍ブロックをコンポーネント化

---
+++ src/components/parts/BookBlock.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import type { BookDetails } from "@/assets/scripts/types";
+
+const props = defineProps<{
+  book: BookDetails
+}>();
+
+console.log(props.book)
+</script>
+
+<template>
+  <div class="c-bookblock">
+    <router-link :to="`/book/?id=${book['id_book']}`">
+      <div class="c-bookblock__inner">
+        <img class="c-bookblock__cover" :src="book['img_url']" />
+        <div>
+          <div class="c-bookblock__title">{{ book['name_book'] }}</div>
+          <dl class="c-bookblock__item">
+            <dt>著者</dt>
+            <dd>{{ book['author'] }}</dd>
+          </dl>
+          <dl class="c-bookblock__item">
+            <dt>出版社</dt>
+            <dd>{{ book['publisher'] }}</dd>
+          </dl>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.c-bookblock {
+  margin-bottom: 20px;
+  color: var(--text-main);
+  background-color: var(--bg-main);
+  padding: 15px;
+  border-radius: 6px;
+  &__inner {
+    display: flex;
+    align-items: flex-start;
+  }
+  &__cover {
+    width: 90px;
+    flex-shrink: 0;
+    margin-right: 15px;
+  }
+  &__title {
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 10px;
+  }
+  // .c-bookinfo__itemと同一
+  &__item {
+    display: flex;
+    align-items: flex-start;
+    margin-bottom: 10px;
+    dt {
+      width: 36px;
+      flex-shrink: 0;
+      padding: 2px 0;
+      text-align: center;
+      font-size: 10px;
+      font-weight: 400;
+      color: var(--text-light);
+      background-color: var(--bg-light);
+      border-radius: 2px;
+      margin-right: 6px;
+    }
+    dd {
+      font-size: 12px;
+      font-weight: 600;
+      padding: 1px 0;
+      color: var(--text-light);
+    }
+  }
+}
+</style>
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -3,6 +3,7 @@ import { useRoute } from 'vue-router';
 import { fetchBookApi } from "@/assets/scripts/api"
 import type { TopCategoryResponse, SubCategoryResponse } from "@/assets/scripts/api"
 import NavigationHeader from '@/components/common/NavigationHeader.vue';
+import PartsBookBlock from '@/components/parts/BookBlock.vue';
 
 // クエリから書籍IDを取得する
 const route = useRoute();
@@ -11,10 +12,10 @@ const subCategoryId: string = route.query.sub;
 
 const topCateogyList: Array<TopCategoryResponse> = await fetchBookApi();
 
-const topCategory: TopCategoryResponse = topCateogyList
+const topCategory: TopCategoryResponse | undefined = topCateogyList
   .find(topCategory => topCategory['id_top_category'] === topCategoryId)
 
-const subCategory: SubCategoryResponse = topCategory['sub_category_list']
+const subCategory: SubCategoryResponse | undefined = topCategory['sub_category_list']
   .find(subCategory => subCategory['id_category'] === subCategoryId)
 
 const topCategoryName = topCategory['name_category'];
@@ -25,27 +26,11 @@ const books = subCategory['book_list']
 <template>
   <div class="p-result">
     <NavigationHeader>
-      {{ topCategoryName }} <img src="@/assets/images/chevron-right.svg"> {{ subCategoryName }}
+      {{ topCategoryName }} − {{ subCategoryName }}
     </NavigationHeader>
     <ul>
-      <li v-for="book in books" :key="book['book_id']" class="c-bookblock">
-        <img :src="book['img_url']" />
-        <div>
-          <div class="c-bookblock__title">{{ book['name_book'] }}</div>
-          <dl class="c-bookblock__item">
-            <dt>著者</dt>
-            <dd>{{ book['author'] }}</dd>
-          </dl>
-          <dl class="c-bookblock__item">
-            <dt>出版社</dt>
-            <dd>{{ book['publisher'] }}</dd>
-          </dl>
-          <dl class="c-bookblock__item">
-            <dt>出版日</dt>
-            <dd>{{ book['create_at'] }}</dd>
-          </dl>
-        </div>
-        
+      <li v-for="book in books" :key="book['book_id']">
+        <PartsBookBlock :book="book" />
       </li>
     </ul>
   </div>
@@ -58,51 +43,12 @@ const books = subCategory['book_list']
     max-width: 480px;
     padding: 20px 15px 60px;
     margin: 0 auto;
-  }
-  .c-bookblock {
-    display: flex;
-    align-items: flex-start;
-    margin-bottom: 20px;
-    background-color: var(--bg-main);
-    padding: 15px;
-    border-radius: 10px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-    img {
-      width: 90px;
-      flex-shrink: 0;
-      margin-right: 15px;
-    }
-    &__title {
-      font-size: 16px;
-      font-weight: 500;
-      margin-bottom: 10px;
-    }
-    // .c-bookinfo__itemと同一
-    &__item {
-      display: flex;
-      align-items: flex-start;
-      margin-bottom: 10px;
-      dt {
-        width: 36px;
-        flex-shrink: 0;
-        padding: 2px 0;
-        text-align: center;
-        font-size: 10px;
-        font-weight: 400;
-        color: var(--text-light);
-        background-color: var(--bg-light);
-        border-radius: 2px;
-        margin-right: 6px;
+    li {
+      border-radius: 10px;
+      &:last-child {
+        margin-bottom: 0;
       }
-      dd {
-        font-size: 12px;
-        font-weight: 600;
-        padding: 1px 0;
-        color: var(--text-light);
-      }
-  }
+    }
   }
 }
 
